9e11d227b3ef94e4b9d570045a2786609c6c6dba
Bingo <Bingo@DUSAN3>
2023-10-21 13:25:03 +0200

    Add project files.

---
+++ R2_T01_01_Int64_05_Komb_Broj_kombinacija_N_nad_K_i_Fact/Program.cs
@@ -0,0 +1,24 @@
+﻿// https://petlja.org/biblioteka/r/Zbirka2/broj_kombinacija
+// https://arena.petlja.org/competition/r1-04-metode-01-staticke-metode#tab_132244
+
+using System;
+
+class Broj_kombinacija_N_nad_K_Ver_000
+{
+    static int fakt(int n)
+    {
+        int p = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            p = p * i;
+        }
+        return p;
+    }
+    static void Main()
+    {
+        int k = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        int c = fakt(n) / (fakt(n - k) * fakt(k));
+        Console.WriteLine(c);
+    }
+}
